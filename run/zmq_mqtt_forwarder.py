27459f7a2480c909012befcb8a411a0919795fb7
Aljoscha Sander <aljoscha@flucto.tech>
2024-01-12 19:38:40 +0000

    finished zmq mqtt forwarder

--- run/zmq_mqtt_forwarder.py
+++ run/zmq_mqtt_forwarder.py
@@ -1,25 +1,28 @@
-import json
 import sys
 
+from heisskleber import get_source
 from heisskleber.config import load_config
 from heisskleber.mqtt import MqttConf, MqttPublisher
-from heisskleber.zmq import ZmqConf, ZmqSubscriber
+
+
+def map_topic(mapping, zmq_topic):
+    return mapping + zmq_topic
 
 
 def main():
-    zmq_conf = load_config(ZmqConf(), "zmq")
-    zmq_source = ZmqSubscriber(zmq_conf, "")
+    # zmq_conf = load_config(ZmqConf(), "zmq")
+    # zmq_source = ZmqSubscriber(zmq_conf, "")
+    zmq_source = get_source("zmq", "")
 
     mqtt_conf = load_config(MqttConf(), "mqtt")
     mqtt_sink = MqttPublisher(mqtt_conf)
     # not sure if this works?
-    mqtt_sink.pack = lambda x: x
-    zmq_source.unpack = lambda x: x
-
+    # mqtt_sink.pack = lambda x: x
+    # zmq_source.unpack = lambda x: x
     try:
         while True:
-            for topic, data in zmq_source.receive():
-                mqtt_sink.send(data=json.loads(data), topic=topic)
+            topic, data = zmq_source.receive()
+            mqtt_sink.send(data, map_topic(mqtt_conf.mapping, topic))
     except KeyboardInterrupt:
         print("received SIGINT, bye")
         sys.exit(0)
