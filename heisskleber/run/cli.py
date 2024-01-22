bf2de2e517829bed8f149afdabf8708895e0857c
Felix Weiler-Detjen <felix@flucto.tech>
2024-01-22 17:11:18 +0100

    Additioal cmdline args for hkcli

--- heisskleber/run/cli.py
+++ heisskleber/run/cli.py
@@ -8,15 +8,17 @@ from heisskleber.console.sink import ConsoleSink
 TopicType = Union[str, list[str]]
 
 
-def parse_args():
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--type", type=str, choices=["zmq", "mqtt", "serial"], default="zmq")
     parser.add_argument("--topic", type=str, default="#")
+    parser.add_argument("--host", type=str, default="localhost")
+    parser.add_argument("--port", type=int, default=1883)
 
     return parser.parse_args()
 
 
-def main():
+def main() -> None:
     args = parse_args()
     source = get_source(args.type, args.topic)
     sink = ConsoleSink()
