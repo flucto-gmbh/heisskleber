aa4bb08f28a01feba1f7394ea5b3fc3735ef0494
Felix Weiler-Detjen <felix@flucto.tech>
2024-02-16 18:16:00 +0800

    Add start, stop and __repr__ to sink and source types.

--- heisskleber/core/types.py
+++ heisskleber/core/types.py
@@ -29,6 +29,24 @@ class Sink(ABC):
         """
         pass
 
+    @abstractmethod
+    def __repr__(self) -> str:
+        pass
+
+    @abstractmethod
+    def start(self) -> None:
+        """
+        Start any background processes and tasks.
+        """
+        pass
+
+    @abstractmethod
+    def stop(self) -> None:
+        """
+        Stop any background processes and tasks.
+        """
+        pass
+
 
 class Source(ABC):
     """
@@ -53,25 +71,21 @@ class Source(ABC):
         """
         pass
 
-
-class AsyncSubscriber(ABC):
-    """
-    AsyncSubscriber interface
-    """
+    @abstractmethod
+    def __repr__(self) -> str:
+        pass
 
     @abstractmethod
-    def __init__(self, config: Any, topic: str | list[str]) -> None:
+    def start(self) -> None:
         """
-        Initialize the subscriber with a topic and a configuration object.
+        Start any background processes and tasks.
         """
         pass
 
     @abstractmethod
-    async def receive(self) -> tuple[str, dict[str, Serializable]]:
+    def stop(self) -> None:
         """
-        Blocking function to receive data from the implemented input stream.
-
-        Data is returned as a tuple of (topic, data).
+        Stop any background processes and tasks.
         """
         pass
 
@@ -97,6 +111,24 @@ class AsyncSource(ABC):
         """
         pass
 
+    @abstractmethod
+    def __repr__(self) -> str:
+        pass
+
+    @abstractmethod
+    def start(self) -> None:
+        """
+        Start any background processes and tasks.
+        """
+        pass
+
+    @abstractmethod
+    def stop(self) -> None:
+        """
+        Stop any background processes and tasks.
+        """
+        pass
+
 
 class AsyncSink(ABC):
     """
@@ -116,5 +148,23 @@ class AsyncSink(ABC):
     async def send(self, data: dict[str, Any], topic: str) -> None:
         """
         Send data via the implemented output stream.
+        """
+        pass
+
+    @abstractmethod
+    def __repr__(self) -> str:
+        pass
+
+    @abstractmethod
+    def start(self) -> None:
+        """
+        Start any background processes and tasks.
+        """
+        pass
+
+    @abstractmethod
+    def stop(self) -> None:
+        """
+        Stop any background processes and tasks.
         """
         pass
