37a29b5c726215fe4e6a26591490b159c7237624
Felix Weiler-Detjen <felix@flucto.tech>
2024-02-17 18:46:02 +0800

    First draft of async udp source.

--- heisskleber/udp/subscriber.py
+++ heisskleber/udp/subscriber.py
@@ -1,10 +1,12 @@
+import asyncio
 import socket
 import sys
 import threading
 from queue import Queue
+from typing import Any
 
 from heisskleber.core.packer import get_unpacker
-from heisskleber.core.types import Serializable, Source
+from heisskleber.core.types import AsyncSource, Serializable, Source
 from heisskleber.udp.config import UdpConf
 
 
@@ -48,6 +50,61 @@ class UdpSubscriber(Source):
             except Exception as e:
                 error_message = f"Error in UDP listener loop: {e}"
                 print(error_message)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(host={self.config.host}, port={self.config.port})"
+
+
+class UdpProtocol(asyncio.DatagramProtocol):
+    def __init__(self, queue: asyncio.Queue[bytes]) -> None:
+        super().__init__()
+        self.queue = queue
+
+    def datagram_received(self, data: bytes, addr: tuple[str | Any, int]) -> None:
+        self.queue.put_nowait(data)
+
+
+class AsyncUdpSubscriber(AsyncSource):
+    """
+    An asynchronous UDP subscriber based on asyncio.protocols.DatagramProtocol
+    """
+
+    def __init__(self, config: UdpConf, topic: str = "udp"):
+        self.config = config
+        self.topic = topic
+        self.unpacker = get_unpacker(self.config.packer)
+        self.queue: asyncio.Queue[bytes] = asyncio.Queue(maxsize=self.config.max_queue_size)
+        self.task: asyncio.Task[None] | None = None
+        self.is_connected = False
+
+    async def setup(self) -> None:
+        loop = asyncio.get_event_loop()
+        self.transport, self.protocol = await loop.create_datagram_endpoint(
+            lambda: UdpProtocol(self.queue),
+            local_addr=(self.config.host, self.config.port),
+        )
+        self.is_connected = True
+
+    def start(self) -> None:
+        pass
+
+    def stop(self) -> None:
+        self.transport.close()
+
+    async def receive(self) -> tuple[str, dict[str, Serializable]]:
+        if not self.is_connected:
+            await self.setup()
+
+        data = await self.queue.get()
+        try:
+            payload = self.unpacker(data.decode(self.config.encoding))
+        except UnicodeDecodeError:
+            print(f"Could not decode data, is not {self.config.encoding}")
+            sys.exit(-1)
+        except Exception:
+            print(f"Could not deserialize data: {data!r}")
+            sys.exit(-1)
+        return (self.topic, payload)
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(host={self.config.host}, port={self.config.port})"
