2f3c67348739a3f15ec98bb0d209902f323653df
Felix Weiler-Detjen <felix@flucto.tech>
2024-03-03 13:55:42 +0800

    Added AsyncTcpSource.

---
+++ heisskleber/tcp/source.py
@@ -0,0 +1,61 @@
+import asyncio
+from typing import Callable
+
+from heisskleber.core.types import AsyncSource, Serializable
+from heisskleber.tcp.config import TcpConf
+
+
+class AsyncTcpSource(AsyncSource):
+    """
+    Async TCP connection, connects to host:port and reads byte encoded strings.
+
+
+    Pass an unpack function like so:
+
+    Example
+    -------
+    def unpack(data: bytes) -> tuple[str, dict[str, float | int | str]]:
+        return dict(zip(["key1", "key2"], data.decode().split(","))
+
+    """
+
+    def __init__(self, config: TcpConf, unpack: Callable[[bytes], tuple[str, dict[str, Serializable]]]):
+        self.config = config
+        self.is_connected = asyncio.Event()
+        self.unpack = unpack
+        self.timeout = config.timeout
+        self.start_task: asyncio.Task[None] | None = None
+
+    async def receive(self) -> tuple[str, dict[str, Serializable]]:
+        await self._check_connection()
+        data = await self.reader.readline()
+        waited = 0.0
+        while data == b"":
+            if waited > self.timeout:
+                raise TimeoutError
+            await asyncio.sleep(0.001)
+            waited += 0.001
+            data = await self.reader.readline()
+        topic, payload = self.unpack(data)
+        return (topic, payload)
+
+    def start(self) -> None:
+        self.start_task = asyncio.create_task(self._connect())
+
+    def stop(self) -> None:
+        if self.is_connected:
+            print("stopping")
+
+    async def _check_connection(self) -> None:
+        if not self.start_task:
+            self.start()
+        await self.is_connected.wait()
+
+    async def _connect(self) -> None:
+        print(f"{self} waiting for connection.")
+        (self.reader, self.writer) = await asyncio.open_connection(self.config.host, self.config.port)
+        print(f"{self} connected successfully!")
+        self.is_connected.set()
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(host={self.config.host}, port={self.config.port})"
