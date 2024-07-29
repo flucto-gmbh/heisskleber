2f159c133613c2ae828e2cabc140f3bc4da54962
Felix Weiler-Detjen <felix@flucto.tech>
2024-07-29 14:14:34 +0200

    Fix start function to async.

--- heisskleber/mqtt/source.py
+++ heisskleber/mqtt/source.py
@@ -30,7 +30,7 @@ class MqttSource(AsyncSource):
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(broker={self.config.host}, port={self.config.port})"
 
-    def start(self) -> None:
+    async def start(self) -> None:
         self._listener_task = create_task(self.run())
 
     def stop(self) -> None:
