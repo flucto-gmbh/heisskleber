2ed23c91e1aceb81705a4f1b71c57249e0c8d474
Felix Weiler-Detjen <felix@flucto.tech>
2024-07-29 14:12:01 +0200

    Rename mqtt source and sink files

--- heisskleber/mqtt/__init__.py
+++ heisskleber/mqtt/__init__.py
@@ -1,5 +1,5 @@
 from .config import MqttConf
-from .publisher_async import AsyncMqttPublisher
-from .subscriber_async import AsyncMqttSubscriber
+from .sink import AsyncMqttPublisher
+from .source import AsyncMqttSubscriber
 
 __all__ = ["MqttConf", "AsyncMqttSubscriber", "AsyncMqttPublisher"]
