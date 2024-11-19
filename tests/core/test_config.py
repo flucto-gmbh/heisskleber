437751f2287a93768513f35a76331cceafc70460
Felix Weiler-Detjen <felix@flucto.tech>
2024-11-19 11:05:19 +0100

    Rename TestConf for pytest naming compability.

--- tests/core/test_config.py
+++ tests/core/test_config.py
@@ -6,14 +6,14 @@ from heisskleber.core import BaseConf
 
 
 @dataclass
-class TestConf(BaseConf):
+class SampleConfig(BaseConf):
     name: str
     age: int = 18
     speed: float = 1.05
 
 
 def test_config_constructor() -> None:
-    test_conf = TestConf(name="Gandalf", age=200, speed=10.0)
+    test_conf = SampleConfig(name="Gandalf", age=200, speed=10.0)
     assert test_conf.name == "Gandalf"
     assert test_conf.age == 200
     assert test_conf.speed == 10.0
@@ -22,33 +22,33 @@ def test_config_constructor() -> None:
 def test_config_from_dict() -> None:
     test_dict = {"name": "Alice", "age": 30, "job": "Electrician", "speed": 1.0}
 
-    expected = TestConf(name="Alice", age=30, speed=1.0)
+    expected = SampleConfig(name="Alice", age=30, speed=1.0)
 
-    configuration = TestConf.from_dict(test_dict)
+    configuration = SampleConfig.from_dict(test_dict)
     assert configuration == expected
 
 
 def test_config_from_dict_with_default() -> None:
     test_dict = {"name": "Alice"}
 
-    expected = TestConf(name="Alice", age=18)
+    expected = SampleConfig(name="Alice", age=18)
 
-    configuration = TestConf.from_dict(test_dict)
+    configuration = SampleConfig.from_dict(test_dict)
     assert configuration == expected
 
 
 def test_pytest_raises_type_error() -> None:
     with pytest.raises(TypeError):
-        TestConf(name=1.0)  # type: ignore[arg-type]
+        SampleConfig(name=1.0)  # type: ignore[arg-type]
 
 
 def test_pytest_raises_type_error_from_dict() -> None:
     with pytest.raises(TypeError):
-        TestConf.from_dict({"name": 1.0, "age": "monkey"})
+        SampleConfig.from_dict({"name": 1.0, "age": "monkey"})
 
 
 def test_conf_from_file() -> None:
-    test_conf = TestConf.from_file("./tests/core/test_conf.yaml")
+    test_conf = SampleConfig.from_file("./tests/core/test_conf.yaml")
 
     assert test_conf.name == "Frodo"
     assert test_conf.age == 30
