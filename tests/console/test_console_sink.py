66dc4aefc18b6150446734381854ff7968c644e6
Aljoscha Sander <aljoscha@flucto.tech>
2025-01-23 14:17:48 +0100

    fixed failing console test

--- tests/console/test_console_sink.py
+++ tests/console/test_console_sink.py
@@ -1,11 +1,11 @@
 import pytest
 
-from heisskleber.console import ConsoleSender
+from heisskleber.console import ConsoleSender, ConsoleConf
 
 
 @pytest.mark.asyncio
 async def test_console_sink(capsys) -> None:
-    sink = ConsoleSender()
+    sink = ConsoleSender(ConsoleConf())
     await sink.send({"key": 3}, "test")
 
     captured = capsys.readouterr()
